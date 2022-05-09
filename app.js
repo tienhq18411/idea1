ccb666fa520f0aaf2b99b28ea6a4086a98a94d1b
tienhq18411 <tienhqgch18411@fpt.edu.vn>
2022-05-09 21:06:42 +0700

    a

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require('express');
 var app = express();
+var router = express.Router();
+var bodyParser = require('body-parser')
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.listen(3000,() =>{
     console.log('Server on port');
